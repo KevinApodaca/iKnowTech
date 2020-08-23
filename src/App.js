be999cc90e2884cd440c3a6738782e085bee3e2a
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-08-23 13:58:14 -0600

    results screen, move to next question, keep score

--- src/App.js
+++ src/App.js
@@ -2,28 +2,55 @@ import React, { useState, useEffect } from 'react';
 import { Questionaire } from './components';
 
 const TRIVIA_URL = 'https://opentdb.com/api.php?amount=20&category=18&difficulty=hard&type=multiple';
+
+
 function App() {
 
-  /* Grab questions */
+  /* Components of quiz game */
   const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
 
   useEffect(() => {
-    fetch(TRIVIA_URL).then((res) => res.json()).then((data) => {
-      setQuestions(data.results);
-    });
+    fetch(TRIVIA_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestions(data.results);
+      });
   }, []);
 
-  const checkAnswer = (answer) => {
-    /* verify correct and incorrect */
-  }
 
-  return questions.length > 0 ? (
+
+/* Verify correct and incorrect. Update the scores */
+  const checkAnswer = (answer) => {
+    const newIndex = currentIndex + 1
+    setCurrentIndex(currentIndex + 1);
+    if (answer === questions[currentIndex].correct_answer){
+      setScore(score + 1);
+    }
+
+    if (newIndex >= questions.length) {
+      setGameOver(true);
+    }
+  };
+
+  /* Body of the game */
+  return gameOver ? (
+    <h1 className='text-3xl text-white font-bold'>
+      You got {score} out of {questions.length}
+    </h1>
+  ) : (
+    questions.length > 0 ? (
     <div className='container'>
-      <Questionaire data={questions[0]} checkAnswer={checkAnswer}/>
+        <Questionaire 
+          data={questions[currentIndex]} 
+          checkAnswer={checkAnswer}
+        />
     </div>
   ) : (
     <h2 className='text-2xl text-white font-bold'>Loading<i className="fa fa-hourglass-start"></i></h2>
-  );
+  ));
 }
   
 
--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -1,12 +1,5 @@
 import React from 'react';
 
-/* Button component */
-const Button = ({answer, className}) => (
-    <button className={`bg-white p-4 text-blue-800 font-semibold rounded shadow ${className}`}>
-        {answer}
-    </button>
-)
-
 const Questionaire = ({ 
     checkAnswer,
     data: {question, correct_answer, incorrect_answers},
@@ -20,14 +13,21 @@ const Questionaire = ({
                 dangerouslySetInnerHTML={{__html:question}}
                 />
             </div>
-        
             <div className='grid grid-cols-2 gap-6 mt-6'>
-                <Button className={correct_answer === shuffledAnswers[0] ? 'bg-blue-300' : ''} onClick={()=>checkAnswer(shuffledAnswers[0])} answer={shuffledAnswers[0]} />
-                <Button className={correct_answer === shuffledAnswers[1] ? 'bg-blue-300' : ''} onClick={()=>checkAnswer(shuffledAnswers[1])} answer={shuffledAnswers[1]} />
-                <Button className={correct_answer === shuffledAnswers[2] ? 'bg-blue-300' : ''} onClick={()=>checkAnswer(shuffledAnswers[2])} answer={shuffledAnswers[2]} />
-                <Button className={correct_answer === shuffledAnswers[3] ? 'bg-blue-300' : ''} onClick={()=>checkAnswer(shuffledAnswers[3])} answer={shuffledAnswers[3]} />
+                {shuffledAnswers.map((answer) => (
+                    <button
+                        className={`${correct_answer
+                             === answer ? 'bg-blue-300' :
+                              'bg-white'} p-4 
+                              text-blue-800 font-semibold 
+                              rounded shadow`}
+                              onClick={()=>checkAnswer
+                              (answer)}
+                              dangerouslySetInnerHTML={{__html: answer}}/>
+                ))}
             </div>
         </div>
-     )};
+     );
+};
 
 export default Questionaire;
