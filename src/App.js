cfb04456b2d2643ecfabd9b04d94b236c46c7220
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-08-23 12:36:21 -0600

    fetch q and a from api

--- src/App.js
+++ src/App.js
@@ -8,32 +8,37 @@ function App() {
 
   useEffect(() => {
     fetch(TRIVIA_URL).then((res) => res.json()).then((data) => {
-      console.log(data);
+      setQuestions(data.results);
     });
   }, []);
 
-  return <div className='container'>
-        <div className='bg-white text-blue-700 p-10 rounded-lg shadow-md'>
-          <h2 className='text-2xl'>The question is going to be here and might be long lets see how it looks</h2>
-          </div>
+  return questions.length > 0 ? (
+    <div className='container'>
+      <div className='bg-white text-blue-800 p-10 rounded-lg shadow-md'>
+        <h2 className='text-2xl'>
+          {questions[0].question}
+        </h2>
+      </div>
 
-       <div className="grid grid-cols-2 gap-6 mt-4">
-         <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
-           Choice A
-         </button>
-         <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
-           Choice B
-         </button>
-         <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
-           Choice C
-         </button>
-         <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
-           Choice D
-         </button>
-       </div>
-        
-
-  </div>;
+      <div className='grid grid-cols-2 gap-6 mt-6'>
+        <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
+          {questions[0].correct_answer}
+        </button>
+        <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
+          {questions[0].incorrect_answers[0]}
+        </button>
+        <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
+          {questions[0].incorrect_answers[1]}
+        </button>
+        <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
+          {questions[0].incorrect_answers[2]}
+        </button>
+      </div>
+    </div>
+  ) : (
+    <h1> loading</h1>
+  );
 }
+  
 
 export default App;
