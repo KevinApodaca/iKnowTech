0220d7bb478820567158c3e090102a885e9db663
Kevin Apodaca <35210520+KevinApodaca@users.noreply.github.com>
2020-08-23 12:51:36 -0600

    refactor into component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Questionaire } from './components';
 
 const TRIVIA_URL = 'https://opentdb.com/api.php?amount=20&category=18&difficulty=hard&type=multiple';
 function App() {
@@ -14,29 +15,10 @@ function App() {
 
   return questions.length > 0 ? (
     <div className='container'>
-      <div className='bg-white text-blue-800 p-10 rounded-lg shadow-md'>
-        <h2 className='text-2xl'>
-          {questions[0].question}
-        </h2>
-      </div>
-
-      <div className='grid grid-cols-2 gap-6 mt-6'>
-        <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
-          {questions[0].correct_answer}
-        </button>
-        <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
-          {questions[0].incorrect_answers[0]}
-        </button>
-        <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
-          {questions[0].incorrect_answers[1]}
-        </button>
-        <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
-          {questions[0].incorrect_answers[2]}
-        </button>
-      </div>
+      <Questionaire data={questions[0]}/>
     </div>
   ) : (
-    <h1> loading</h1>
+    <h2 className='text-2xl text-white font-bold'>Loading<i className="fa fa-hourglass-start"></i></h2>
   );
 }
   
---
+++ src/components/Questionaire.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Questionaire = ({ data: {question, correct_answer, incorrect_answers} }) => (
+
+    <div>
+        <div className='bg-white text-blue-800 p-10 rounded shadow-md'>
+            <h2 
+              className='text-2xl'
+              dangerouslySetInnerHTML={{__html:question}}
+              />
+          </div>
+    
+          <div className='grid grid-cols-2 gap-6 mt-6'>
+            <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
+              {correct_answer}
+            </button>
+            <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
+              {incorrect_answers[0]}
+            </button>
+            <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
+              {incorrect_answers[1]}
+            </button>
+            <button className='bg-white p-4 text-blue-800 font-semibold rounded shadow'>
+              {incorrect_answers[2]}
+            </button>
+          </div>
+    </div>
+);
+
+export default Questionaire;
